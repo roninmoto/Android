0fc58387a8aa31e1ae7cc26ea15a37a424a4cca4
Joel Betterly <roninmoto@gmail.com>
2013-05-16 10:27:13 -0400

    changed header

--- Java II/Week 2/src/com/jbetterly/auto2/CarDisplay.java
+++ Java II/Week 2/src/com/jbetterly/auto2/CarDisplay.java
@@ -2,12 +2,11 @@
  * Project: AutoJava2
  * Package: com.jbetterly.auto2
  * @author: Joel Betterly
- * Date: 	May 8, 2013
+ * Date: 	May 16, 2013
  */
 package com.jbetterly.auto2;
 
 import android.content.Context;
-import android.graphics.Color;
 import android.widget.GridLayout;
 import android.widget.TextView;
 
