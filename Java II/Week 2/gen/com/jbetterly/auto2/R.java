f621402797006e7d322a1daa6202bf7f0e1b7f9c
Joel Betterly <roninmoto@gmail.com>
2013-05-16 08:42:58 -0400

    autogenerated

--- Java II/Week 2/gen/com/jbetterly/auto2/R.java
+++ Java II/Week 2/gen/com/jbetterly/auto2/R.java
@@ -29,23 +29,27 @@ public final class R {
         public static final int _modelName=0x7f080003;
         public static final int _vin=0x7f080005;
         public static final int _year=0x7f080002;
-        public static final int action_settings=0x7f080010;
-        public static final int btnImplicit=0x7f08000f;
-        public static final int button1=0x7f08000e;
+        public static final int action_settings=0x7f080012;
+        public static final int btnImplicit=0x7f080011;
+        public static final int button1=0x7f080010;
+        public static final int form_fragment=0x7f08000d;
         public static final int frameLayout1=0x7f080000;
         public static final int goButton=0x7f08000b;
         public static final int infoPage=0x7f08000c;
+        public static final int info_fragment=0x7f08000e;
         public static final int modelLabel=0x7f080006;
         public static final int myGrid=0x7f080001;
         public static final int nameLabel=0x7f080007;
         public static final int productField=0x7f08000a;
-        public static final int textView1=0x7f08000d;
+        public static final int textView1=0x7f08000f;
         public static final int vinLabel=0x7f080009;
         public static final int yearLabel=0x7f080008;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int infopage=0x7f030001;
+        public static final int formfrag=0x7f030001;
+        public static final int infofrag=0x7f030002;
+        public static final int infopage=0x7f030003;
     }
     public static final class menu {
         public static final int main=0x7f070000;
